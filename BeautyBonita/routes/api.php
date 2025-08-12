4f067bfc0e5a12c115308e717321d8710a040aa2
Carlos3099 <st3099@utr.edu.mx>
2025-08-12 16:03:19 -0600

    README

--- BeautyBonita/routes/api.php
+++ BeautyBonita/routes/api.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::post('/empleados/{empleado}/huellas', [HuellaController::class, 'store']);
+Route::post('/asistencias', [AsistenciaController::class, 'store']);
+
+// Catálogo para identificación local (la app .NET lo consume)
+Route::get('/catalogo-fmds', function () {
+  return \App\Models\Huella::select('empleado_id','fmd_xml')->get();
+});
