5cc76b22e4a1cb606bf25926403d65b8f06d99b2
Carlos3099 <st3099@utr.edu.mx>
2025-08-27 13:32:03 -0600

    mejoras para vite

--- BeautyBonita/vite.config.js
+++ BeautyBonita/vite.config.js
@@ -3,6 +3,15 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',   // 👈 expone Vite a la red (no solo dentro del contenedor)
+        port: 5173,        // 👈 fija el puerto (no cambiará a 5174)
+        strictPort: true,  // 👈 lanza error si el 5173 ya está ocupado
+        hmr: {
+            host: 'localhost', // 👈 usa localhost en tu navegador
+            port: 5173,
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
